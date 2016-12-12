0561b3dd6411fa8d3b1711c74d44141af7110451
Ped90 <Ped90@Ped90>
2016-12-12 09:01:43 +0100

    Una vez creado llamamos a la clase pedirdatos y calculardatos.

--- lovivido/src/lovivido/calculo.java
+++ lovivido/src/lovivido/calculo.java
@@ -5,6 +5,7 @@ public class calculo {
     public static void main(String[] args) {
     
         Lovivido per1 = new Lovivido();
-        
+        per1.pedirdatos();
+        per1.calculardatos();
 }
 }
