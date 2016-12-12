c36fdef7881dfa160d60cb6aa4c5a03669ffa59e
Ped90 <Ped90@Ped90>
2016-12-12 09:05:45 +0100

    Pasamos el main para una clase nueva llamada calc

---
+++ factorial/src/factorial/Factorial.java
@@ -0,0 +1,36 @@
+
+package factorial;
+
+public class Factorial {
+           
+    int j;
+    int f;
+
+  
+   j = 8;    
+   
+
+    
+   int i;
+   if (j == 0)
+f = 1;
+   else
+   {
+f = 1;
+for (i = j; i >= 1; i--)
+{
+    f = f * i;
+}
+   }    
+  
+ 
+
+System.out.println(f); 
+
+
+    }
+    
+}
+    
+    
+
---
+++ factorial/src/factorial/calc.java
@@ -0,0 +1,7 @@
+
+package factorial;
+
+public class calc {
+    public static void main(String[] args) {
+    
+}
