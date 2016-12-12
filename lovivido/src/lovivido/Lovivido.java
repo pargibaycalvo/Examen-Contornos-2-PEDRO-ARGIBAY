9e124e56fbfd85e0ec5504873acc35e7a577c905
Ped90 <Ped90@Ped90>
2016-12-12 08:59:02 +0100

    Verificamos que hace el comando "a = Integer.parseInt(edad)" y le cambiamos el nombre.

--- lovivido/src/lovivido/Lovivido.java
+++ lovivido/src/lovivido/Lovivido.java
@@ -7,22 +7,22 @@ import javax.swing.JOptionPane;
 public class Lovivido {
        
 String nombre, edad;
-int mes,dia,hora,a;
+int mes,dia,hora,numentero;
 
 
 public void pedirdatos(){
 nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
 edad = JOptionPane.showInputDialog ("Escriba su edad: ");
-a = Integer.parseInt (edad);
+numentero = Integer.parseInt (edad);
 }
 
 public void calculardatos(){
 
 JOptionPane.showMessageDialog(null,  "Meses: "  +mes,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE); 
-mes = (a*12);
+mes = (numentero*12);
 
 JOptionPane.showMessageDialog(null, "Días: " +dia,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
-dia = (a*365);
+dia = (numentero*365);
 
 JOptionPane.showMessageDialog(null, "Horas: " +hora,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
 hora = (dia*24);
